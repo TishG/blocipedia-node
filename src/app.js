ae24ba0a51a7abd66c4da10e0de188163bbc47b6
Tish <latisha.griffiths@gmail.com>
2018-12-01 13:04:53 -0600

    Removed appConfig, will implement later

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-const appConfig = require("./db/config/main-config.js");
+// const appConfig = require("./db/config/main-config.js");
 
-appConfig.init();
+// appConfig.init();
 
 app.use("/", (req, res, next) => {
     res.send("Welcome to Blocipedia")
