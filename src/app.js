15289c91d5650d1d04af10619f67de8caf401b5a
Tish <latisha.griffiths@gmail.com>
2018-12-01 12:40:25 -0600

    Completed user story: As a developer, I want to scaffold my project and set up my project for deployment

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const appConfig = require("./db/config/main-config.js");
+
+appConfig.init();
+
+app.use("/", (req, res, next) => {
+    res.send("Welcome to Blocipedia")
+  });
+
+module.exports = app;
