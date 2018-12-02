446a56ab0771b0959c947d18450062fd5a18c9bf
Tish <latisha.griffiths@gmail.com>
2018-12-01 22:12:14 -0600

    Added landing page

--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -1,14 +1,5 @@
-// module.exports = {
-//     index(req, res, next){
-//         res.render("static/index", {title: "Welcome to Blocipedia"});
-//     }
-//   }
-
-const express = require('express');
-const router = express.Router();
-
 module.exports = {
-    index(req, res, next){
-        res.render("index.ejs", {title: "Welcome to Bloccit"});
+    index(req, res, next) {
+        res.render("static/index");
     }
-}
+  }
