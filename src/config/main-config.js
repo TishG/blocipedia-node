1c153dfc94cd4cae37414a8064427442ef46c216
Tish <latisha.griffiths@gmail.com>
2018-12-03 11:39:38 -0600

    pushing work to reposiory for review

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 const path = require("path");
 const viewsFolder = path.join(__dirname, "..", "views");
 const logger = require('morgan');
+const expressValidator = require("express-validator");
+const session = require("express-session");
+const flash = require("express-flash");
 
 module.exports = {
   init(app, express){
@@ -9,6 +12,14 @@ module.exports = {
     app.set("view engine", "ejs");
     app.use(express.static(path.join(__dirname, "..", "assets")));
     app.use(logger('dev'));
+    app.use(expressValidator());
+    app.use(session({
+      secret: process.env.cookieSecret,
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 60000 }
+    }));
+    app.use(flash());
   }
 };
 
