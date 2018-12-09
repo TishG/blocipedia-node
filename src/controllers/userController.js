dbf81a70a2c2f71b6179c7f3586ab3da71f9753b
Tish <latisha.griffiths@gmail.com>
2018-12-09 01:34:51 -0600

    Implemented sendGrid in queries.users.js (not complete)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,6 @@
 const userQueries = require("../db/queries.users.js");
 const passport = require("passport");
-const sgMail = require('@sendgrid/mail');
+// const sgMail = require('@sendgrid/mail');
 
 module.exports = {
     signUp(req, res, next) {
@@ -37,34 +37,34 @@ module.exports = {
               res.redirect("/");
             }
           })
-        },
-        sendEmail(req, res, next) {
-          let newUser = {
-            email: req.body.email,
-            password: req.body.password,
-            passwordConfirmation: req.body.passwordConfirmation
-          };
-          const msg = {
-              // to: req.body.email,
-              to: newUser.email,
-              from: 'noreply@blocipedia.com',
-              subject: 'Welcome to Blocipedia!',
-              text: 'Thank you joining blocipedia!, sign up to view your profile.',
-              html: '<strong>Thank you joining blocipedia!, sign up to view your profile.</strong>',
-          };
-          userQueries.createUser(newUser, (err, user) => {
-          if(err){
-              req.flash("error", err);
-              res.redirect("/users/sign_up");
-          } else {
-              passport.authenticate("local")(req, res, () => {
-                  req.flash("notice", "You've successfully signed in!");
-                  sgMail.setApiKey('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');
-                  sgMail.send(msg);
-                  res.redirect("/");
-              })
-          }
-      })
-    }
+        }
+    //     sendEmail(req, res, next) {
+    //       let newUser = {
+    //         email: req.body.email,
+    //         password: req.body.password,
+    //         passwordConfirmation: req.body.passwordConfirmation
+    //       };
+    //       const msg = {
+    //           // to: req.body.email,
+    //           to: newUser.email,
+    //           from: 'noreply@blocipedia.com',
+    //           subject: 'Welcome to Blocipedia!',
+    //           text: 'Thank you joining blocipedia!, sign up to view your profile.',
+    //           html: '<strong>Thank you joining blocipedia!, sign up to view your profile.</strong>',
+    //       };
+    //       userQueries.createUser(newUser, (err, user) => {
+    //       if(err){
+    //           req.flash("error", err);
+    //           res.redirect("/users/sign_up");
+    //       } else {
+    //           passport.authenticate("local")(req, res, () => {
+    //               req.flash("notice", "You've successfully signed in!");
+    //               sgMail.setApiKey('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');
+    //               sgMail.send(msg);
+    //               res.redirect("/");
+    //           })
+    //       }
+    //   })
+    // }
 
 }
--- src/db/queries.users.js
+++ src/db/queries.users.js
@@ -1,5 +1,7 @@
 const User = require("./models").User;
 const bcrypt = require("bcryptjs");
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 module.exports = {
 
@@ -13,10 +15,19 @@ module.exports = {
       password: hashedPassword
     })
     .then((user) => {
+      const msg = {
+        to: newUser.email,
+        from: 'no-reply@blocipedia.con',
+        subject: 'Welcome to Blocipedia',
+        text: 'This is an email confirmation that you have signed up for Blocipedia. Log in to start creating content!',
+        html: '<strong>This is an email confirmation that you have signed up for Blocipedia. Log in to start creating content!</strong>',
+      };
+      sgMail.send(msg);
       callback(null, user);
     })
     .catch((err) => {
       callback(err);
+      console.log(err);
     })
   }
 
