4a28b902e68ca52a888c4ad06a8b5071e602ccf9
Tish <latisha.griffiths@gmail.com>
2018-12-09 02:28:58 -0600

    Completed Checkpoint: User Sign In and Out

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -37,6 +37,11 @@ module.exports = {
               res.redirect("/");
             }
           })
+        },
+        signOut(req, res, next){
+          req.logout();
+          req.flash("notice", "You've successfully signed out!");
+          res.redirect("/");
         }
     //     sendEmail(req, res, next) {
     //       let newUser = {
