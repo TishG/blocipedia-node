05952b3b2c18c872a3d34b08c933144209ad8c7e
Tish <latisha.griffiths@gmail.com>
2018-12-10 13:50:49 -0600

    Working on the create action

--- spec/integration/wikis_spec.js
+++ spec/integration/wikis_spec.js
@@ -5,7 +5,7 @@ const sequelize = require("../../src/db/models/index").sequelize;
 const Wiki = require("../../src/db/models").Wiki;
 const User = require("../../src/db/models").User;
 
-describe("routes : posts", () => {
+describe("routes : wikis", () => {
 
         beforeEach((done) => {
           this.user;
@@ -18,8 +18,8 @@ describe("routes : posts", () => {
           .then((user) => {
             this.user = user;
             Wiki.create({
-              title: "My first visit to Proxima Centauri b",
-              body: "I saw some rocks.",
+              title: "JS Frameworks",
+              body: "There is a lot of them",
               userId: this.user.id
             })
             .then((wiki) => {
@@ -34,6 +34,26 @@ describe("routes : posts", () => {
         });
       }); 
 
+      describe("GET /wikis", () => {
+        it("should return a status code 200 and all wikis", (done) => {
+                 request.get(base, (err, res, body) => {
+                   expect(res.statusCode).toBe(200);
+                   expect(err).toBeNull();
+                   expect(body).toContain("Wikis");
+                   expect(body).toContain("JS Frameworks");
+                   done();
+                 });
+               });
+        // it("should respond with all wikis", (done) => {
+        //   request.get(base, (err, res, body) => {
+        //     expect(err).toBeNull();
+        //     expect(body).toContain("Wikis");
+        //     expect(body).toContain("My first visit to Proxima Centauri b");
+        //     done();
+        //   });
+        // });
+      });
+
   describe("GET /wikis/new", () => {
 
     it("should render a new wiki form", (done) => {
@@ -42,6 +62,37 @@ describe("routes : posts", () => {
         expect(body).toContain("New Wiki");
         done();
       });
+    });
+  });
+
+  describe("POST /wikis/create", () => {
+    const options = {
+      url: `${base}create`,
+      form: {
+        title: "blink-182 songs",
+        body: "What's your favorite blink-182 song?",
+        private: false,
+        userId: 1
+      }
+    };
+    it("should create a new wiki and redirect", (done) => {
+      request.post(options,
+        (err, res, body) => {
+          Wiki.findOne({where: {title: "blink-182 songs"}})
+          .then((wiki) => {
+            expect(res.statusCode).toBe(303);
+            expect(wiki.title).toBe("blink-182 songs");
+            expect(wiki.body).toBe("What's your favorite blink-182 song?");
+            expect(wiki.private).toBe(false);
+            expect(wiki.userId).toBe(1);
+            done();
+          })
+          .catch((err) => {
+            console.log(err);
+            done();
+          });
+        }
+      );
     });
   });
 
--- src/controllers/wikiController.js
+++ src/controllers/wikiController.js
@@ -2,15 +2,31 @@ const wikiQueries = require("../db/queries.wikis.js");
 
 module.exports = {
     index(req, res, next){
-        wikiQueries.getAllWikis((err, wikis) => {
-                if(err){
-                  res.redirect(500, "/");
-                } else {
-                res.render("wikis/index", {wikis});
-              }
-            })
-          },
+      wikiQueries.getAllWikis((err, wikis) => {
+          if(err){
+            res.redirect(500, "static/index");
+          } else {
+          res.render("wikis/index", {wikis});
+          }
+        })
+      },
     new(req, res, next){
-        res.render("wikis/new", {userId: req.params.userId});
+        res.render("wikis/new");
+      },
+      create(req, res, next){
+        let newWiki = {
+          title: req.body.title,
+          body: req.body.body,
+          private: req.body.private,
+          userId: this.user.id
+        };
+        wikiQueries.addWiki(newWiki, (err, wiki) => {
+          if(err){
+            res.redirect(500, "/wikis/new");
+          } else {
+            res.redirect(303, `/wikis/${wiki.id}`);
+          }
+        })
       }
-  }
+   
+  }  
