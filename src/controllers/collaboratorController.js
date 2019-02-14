3d0f0d6547ed568863e85bf7ae7b997faa1badb6
Tish <latisha.griffiths@programmer.net>
2019-02-14 12:09:16 -0600

    Made changes to remove method in collaboratorController

--- src/controllers/collaboratorController.js
+++ src/controllers/collaboratorController.js
@@ -18,21 +18,17 @@ module.exports = {
     },
     remove(req, res, next) {
         if(req.user) {
-            // const authorized = new Authorizer(req.user, wikiId, collabId).destroy();
-            const authorized = new Authorizer( req.params.userId ).destroy();
-            if(authorized) {
-                console.log("autohized? " + authoried);
-                console.log("userId: " + req.params.userId);
             collaboratorQueries.removeCollaborator(req, (err, collaborator) => {
                 if(err) {
                     req.flash('error', err);
                 }
-                res.redirect(req.headers.referer);
+                else {
+                  res.redirect(req.headers.referer);
+                }
             })
         } else {
             req.flash('notice', 'You must be signed in to do that.');
             res.redirect(req.headers.referer);
             }
         }
-    }
 }
